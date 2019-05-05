9ade56d18212edbfe3fbea482fcf2c290bc307aa
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2019-05-05 20:17:02 +0300

    enable method security

--- spring-security/src/main/java/com/acme/springsecurity/controller/HelloController.java
+++ spring-security/src/main/java/com/acme/springsecurity/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.acme.springsecurity.controller;
 
 import com.acme.springsecurity.domain.Hello;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,6 +16,14 @@ public class HelloController {
 	public Hello sayHello() {
 
 		return new Hello("Hi, Man");
+
+	}
+
+	@Secured({ "ROLE_POWER_USER" })
+	@GetMapping("/power")
+	public Hello sayHelloPower(Principal principal) {
+
+		return new Hello("Hi, " + principal.getName());
 
 	}
 
